e33d1550f5e29ff3e24d40eb127415a139f817de
LI Yunxiang <1718613239banma@gmail.com>
2019-02-12 20:45:21 +0800

    Create lc94.py

---
+++ lc94.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: 'TreeNode') -> 'List[int]':
+        tree_stack = []
+        tree_stack.append(root)
+        answer = []
+        temp = root
+        while(len(tree_stack)): #TODO
+            temp = tree_stack.pop()
+            if temp.right == None and temp.left == None:
+                answer.append(temp.val)
+            else:
+                if temp.right != None:
+                    tree_stack.append(temp.right)
+                    temp.right = None           
+                tree_stack.append(temp)
+                if temp.left != None:
+                    tree_stack.append(temp.left)
+                    temp.left = None
+                
+            
