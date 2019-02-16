43ec5cadf08db4d5c1efc39fc0b15fc396843b38
LI Yunxiang <1718613239banma@gmail.com>
2019-02-16 17:51:54 +0800

    Create lc70.py

---
+++ lc70.py
@@ -0,0 +1,15 @@
+import operator
+import functools
+class Solution:
+    def ncr(self, n, r):
+        r = min(r, n-r)
+        numer = functools.reduce(operator.mul, range(n, n-r, -1), 1)
+        denom = functools.reduce(operator.mul, range(1, r+1), 1)
+        return numer / denom
+
+    def climbStairs(self, n: 'int') -> 'int':
+        answer = 0
+        for i in range(int(n/2)+1):
+            # print(self.ncr(n-i, n-2*i))
+            answer += self.ncr(n-i, n-2*i)
+        return int(answer)
